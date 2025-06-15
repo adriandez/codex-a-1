e79a24550945f9ed0ba01de2018b54f5c2904a95
Adrian E. de Zamacona Garza <66616997+adriandez@users.noreply.github.com>
2025-06-15 15:23:22 +0200

    Switch to flat ESLint config

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    }
+  }
+];
